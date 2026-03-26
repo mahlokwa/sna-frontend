7b7a810b21b6826ca3b98f9e8dc1717f3c5b9873
mahlokwa <mphkam016@myuct.ac.za>
2026-03-26 08:01:15 +0200

    updated tokenEntry

--- src/pages/TokenEntry.js
+++ src/pages/TokenEntry.js
@@ -21,7 +21,7 @@ function TokenEntry() {
     setError('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/bookings/verify-token', {
+     const res = await fetch(`${process.env.REACT_APP_API_URL}/api/bookings/verify-token`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ token: token.trim().toUpperCase() })
