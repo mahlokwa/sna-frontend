03ef7b69e89fe2e1950463f574941778bc981a03
mahlokwa <mphkam016@myuct.ac.za>
2026-04-10 00:24:42 +0200

    updated Bookings.js

--- src/pages/Bookings.js
+++ src/pages/Bookings.js
@@ -15,9 +15,12 @@ function Bookings() {
   const [errorMessage, setErrorMessage] = useState('');
 
   const timeSlots = [
-    '09:00 AM', '10:00 AM', '11:00 AM',
-    '12:00 PM', '01:00 PM', '02:00 PM',
-    '03:00 PM', '04:00 PM', '05:00 PM',
+  '07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM',
+  '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM',
+  '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM',
+  '01:00 PM', '01:30 PM', '02:00 PM', '02:30 PM',
+  '03:00 PM', '03:30 PM', '04:00 PM', '04:30 PM',
+  '05:00 PM',
   ];
 
   useEffect(() => {
